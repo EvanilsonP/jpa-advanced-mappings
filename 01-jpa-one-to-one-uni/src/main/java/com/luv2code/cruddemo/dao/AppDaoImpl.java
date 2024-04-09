9043a12cfeb921d1b882b0f1caff0f327ad9879e
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-09 14:52:44 -0300

    Create the DAO

---
+++ 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDaoImpl.java
@@ -0,0 +1,24 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AppDaoImpl implements AppDao{
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public AppDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+}
