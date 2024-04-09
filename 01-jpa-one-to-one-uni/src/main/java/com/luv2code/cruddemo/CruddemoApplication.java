acfda56ad105444eb3610fc415ca6302a040543d
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-09 15:02:18 -0300

    Develop the main app

--- 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.AppDao;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,9 +14,21 @@ public class CruddemoApplication {
 	public static void main(String[] args) { SpringApplication.run(CruddemoApplication.class, args); }
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDao appDAO) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDao appDAO) {
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Chad", "Darby", "luv2code@gmail.com");
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("www.luv2code.com/youtube", "luv 2 code");
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+		// save instructor to the database / this'll also save the details object
+		System.out.println("Saved instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
 	}
 }
