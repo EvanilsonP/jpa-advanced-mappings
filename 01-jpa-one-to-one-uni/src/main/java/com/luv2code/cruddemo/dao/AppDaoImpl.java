e7966e43806f85bdcf0924b41b354b41d038aac4
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-12 08:55:09 -0300

    Lazy find courses

--- 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDaoImpl.java
+++ 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDaoImpl.java
@@ -1,11 +1,15 @@
 package com.luv2code.cruddemo.dao;
 
+import com.luv2code.cruddemo.entity.Course;
 import com.luv2code.cruddemo.entity.Instructor;
 import com.luv2code.cruddemo.entity.InstructorDetail;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class AppDaoImpl implements AppDao{
@@ -49,5 +53,14 @@ public class AppDaoImpl implements AppDao{
         InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, theId);
         tempInstructorDetail.getInstructor().setInstructorDetail(null);
         entityManager.remove(tempInstructorDetail);
+    }
+
+    @Override
+    public List<Course> findCoursesByInstructorId(int theId) {
+        TypedQuery<Course> query = entityManager.createQuery(
+                "from Course where instructor.id = :data", Course.class);
+        query.setParameter("data", theId);
+        List<Course> courses = query.getResultList();
+        return courses;
     }
 }
