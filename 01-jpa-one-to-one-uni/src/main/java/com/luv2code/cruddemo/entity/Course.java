2563b67415af1d516b8b3f444fba0fdc774c0763
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-13 09:42:50 -0300

    Refactor Course Entity

--- 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -3,6 +3,10 @@ package com.luv2code.cruddemo.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,10 +27,21 @@ public class Course {
     @JoinColumn(name = "instructor_id")
     private String instructor;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
+
     public Course() {}
 
     public Course(String title) {
         this.title = title;
+    }
+
+    public void add(Review theReview) {
+        if(reviews == null) {
+            reviews = new ArrayList<>();
+        }
+        reviews.add(theReview);
     }
 
     @Override
