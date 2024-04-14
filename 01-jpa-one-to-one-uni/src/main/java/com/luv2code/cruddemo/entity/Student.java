51fa28e8a2fe642a86f8a4acb591eb5dbf0ebdcd
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-14 15:40:57 -0300

    Map Student to Course

--- 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Student.java
+++ 01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Student.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 
@@ -25,7 +28,22 @@ public class Student {
     @Column(name = "email")
     private String email;
 
+    @ManyToMany(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course-student",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private List<Course> courses;
+
     public Student() {}
+
+    public void addCourse(Course course) {
+        if(course == null) {
+            courses = new ArrayList<>();
+        }
+        courses.add(course);
+    }
 
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
